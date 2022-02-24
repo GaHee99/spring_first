fbcb4f40a3d612647ba9a9949fba2271fccd8ef0
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2022-02-24 10:56:07 +0900

    할인정책 변경 + 인터페이스에만 의존하게 변경

--- core/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,4 +1,22 @@
 package hello.core.discount;
 
-public class RateDiscountPolicy {
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+        private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade()== Grade.VIP){
+            //할인된 금액을 리턴..
+            return price * discountPercent / 100;
+        }
+        else{
+            return 0 ;
+        }
+    }
+
+
 }
--- core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,8 +9,8 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+   // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     //주문을 만드는 class
